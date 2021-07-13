31bcddd4d4b2668071526786727e9ba95f1b96b2
Park <pjane123@naver.com>
2021-07-13 23:49:45 +0900

    add apollo/client

---
+++ src/lib/apolloClient.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const prod = process.env.NODE_ENV === 'production';
+
+
+const client = new ApolloClient({
+  uri: prod? "localhost:4004" : "localhost:4004",
+  cache: new InMemoryCache()
+})
+
+export default client;
