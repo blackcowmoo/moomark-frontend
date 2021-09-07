4022c2e0d942f8cf4a2dcc512ce4c66d7570b388
Park <pjane123@naver.com>
2021-09-07 20:45:52 +0900

    feat: Header dropdown close 로직 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,8 +14,14 @@ const Header: React.FC = () => {
   const toggleDropdown = () => {
     setDropdown((prev) => !prev);
   };
+
+  const closeDropdown = () => {
+    setDropdown(false);
+  };
+
   const setLogOut = () => {
     logout();
+    closeDropdown();
   };
 
   return (
@@ -25,17 +31,9 @@ const Header: React.FC = () => {
           <img className={styles.logo} src='/logo.svg' alt='mooMark logo' />
         </Link>
         <form action='#' className={styles.search}>
-          <input
-            type='text'
-            className={styles.search__input}
-            placeholder='Search input'
-          />
+          <input type='text' className={styles.search__input} placeholder='Search input' />
           <button className={styles.search__button}>
-            <img
-              src='/icon/search.svg'
-              alt='search icon'
-              className={styles.search__icon}
-            />
+            <img src='/icon/search.svg' alt='search icon' className={styles.search__icon} />
           </button>
         </form>
         {user ? (
@@ -47,18 +45,12 @@ const Header: React.FC = () => {
               <img src='/icon/bell.svg' alt='User bookmarks' />
             </div>
             <div className={styles.userNav__user}>
-              <img
-                src='/mockprofile.PNG'
-                alt='mockNick'
-                onClick={toggleDropdown}
-              />
+              <img src='/mockprofile.PNG' alt='mockNick' onClick={toggleDropdown} />
               {isDropdown && (
                 <div>
-                  <div className={styles.dropdown}>
+                  <div className={styles.dropdown} onClick={closeDropdown}>
                     <div className={styles.menuWrapper}>
-                      <div className={styles.userInfo}>
-                        {user}님 환영합니다!
-                      </div>
+                      <div className={styles.userInfo}>{user}님 환영합니다!</div>
                       <Link href='/'>마이페이지</Link>
                       <Link href='/'>내 북마크</Link>
                       <Link href='/edit'>새 글 작성</Link>
@@ -77,12 +69,7 @@ const Header: React.FC = () => {
           </button>
         )}
       </div>
-      <Modal
-        title='ddd'
-        isShown={isShown}
-        onClose={toggle}
-        content={<ModalLoginForm onClose={toggle} />}
-      />
+      <Modal title='ddd' isShown={isShown} onClose={toggle} content={<ModalLoginForm onClose={toggle} />} />
     </header>
   );
 };
