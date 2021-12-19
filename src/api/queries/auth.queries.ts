f5d52d95209f761a41830e4d09fc372018a6bee0
Park <pjane123@naver.com>
2021-12-19 23:21:18 +0900

    feat: GoogleSocialLogin Graphql 추가

--- src/api/queries/auth.queries.ts
+++ src/api/queries/auth.queries.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN = gql`
-  mutation {
-    login
+  mutation LogIn($type: String!, $code: String!) {
+    login(type: $type, code: $code) 
   }
 `;
