cf98a0c538ac2ef73e4245d0a07fe46ea8b0da85
micalgenus <micalgenus@gmail.com>
2021-12-28 01:23:40 +0900

    jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   setupFilesAfterEnv: ['./test-setup.ts'],
-  testPathIgnorePatterns: ['<rootDir>/.next/'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '.scss'],
+  testMatch: ['**/__tests__/*.test.(ts|tsx)'],
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)?$': 'ts-jest',
+    '^.+\\.(css|less|scss)$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!variables/.*)'],
+  coverageReporters: ['json', 'text'],
+  collectCoverageFrom: ['src/**/*.ts*'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/.next/'],
+  coverageThreshold: {
+    global: {
+      lines: 10,
+      statements: 10,
+    },
+  },
 };
--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -1,55 +1,9 @@
-import { PostListViewProps } from 'types';
+import { shallow } from 'enzyme';
+import HomePage from '@pages/index';
 
 describe('Page Index', () => {
   it('render index', () => {
-    // const wrapper = shallow(<Index />);
-    // console.log(wrapper);
-
-    // expect(wrapper.find('h1').text()).toBe('mooMark main');
+    const home = shallow(<HomePage />);
+    console.log(home.text());
   });
-});
-
-const date = new Date(2021, 2, 10);
-
-const mock: PostListViewProps[] = [
-  {
-    id: 0,
-    title: 'mockTitle',
-    author: 'gosu',
-    like: 222,
-    tags: ['응애'],
-    date,
-  },
-  {
-    id: 0,
-    title: 'ddd',
-    author: 'gosu',
-    like: 222,
-    tags: ['응애'],
-    date,
-  },
-  {
-    id: 0,
-    title: 'ddd',
-    author: 'gosu',
-    like: 222,
-    tags: ['응애'],
-    date,
-  },
-  {
-    id: 0,
-    title: 'ddd',
-    author: 'gosu',
-    like: 222,
-    tags: ['응애'],
-    date,
-  },
-];
-
-describe('<DefaultPostItem/>', () => {
-  // it('render without carsh', ()=>{
-  //   const wrapper = mount(<DefaultViewItem value={mock[0]}/>);
-  //   expect(wrapper).toMatchSnapshot();
-  // })
-  console.log(mock[0]);
 });
