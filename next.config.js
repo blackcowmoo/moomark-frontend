b038a12eace4b93e9e879b3a7129a2eed38fd9a6
micalgenus <micalgenus@gmail.com>
2021-12-28 23:24:13 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -5,12 +5,13 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  webpack: (config, { webpack }) => {
+  excludeFile: (str) => /\*\.test\.[jt]s(?x)$/.test(str),
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
+    // config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
     if (process.env.NODE_ENV === 'development') config.plugins.push(new Dotenv());
     return config;
   },
