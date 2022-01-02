9d344a873f652f75456151354ff8b97ece3154de
Park <pjane123@naver.com>
2022-01-02 23:02:17 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,13 +8,16 @@ class MainDocument extends Document {
 
   render() {
     const setInitialTheme = `
-    function setUserLocalTheme() {
+    function getUserPreference() {
       if(window.localStorage.getItem('theme')) {
-        return window.localStorage.getItem('theme');
+        return window.localStorage.getItem('theme')
       }
-      return null;
+      return window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? 'dark'
+                : 'light'
     }
-    document.body.dataset.theme = setUserLocalTheme() : 'light';
+    document.body.dataset.theme = getUserPreference();
+   
   `;
     return (
       <Html>
