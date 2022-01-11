7b3408e89102a433d8ee58e86b2fff6828c00c6a
Park <pjane123@naver.com>
2022-01-11 18:38:47 +0900

    Add TilePostList snapshot test

---
+++ src/__tests__/components/PostList/index.test.tsx
@@ -0,0 +1,10 @@
+import { mount } from 'enzyme';
+import TilePostList from '@components/PostList/TileViewPostList';
+import { HomePageTileMock } from 'utils/mock';
+
+describe('<TilePostList/>', () => {
+  it('matches snapshot', () => {
+    const wrapper = mount(<TilePostList listTitle={'인기'} postList={HomePageTileMock}/>);
+    expect(wrapper.debug()).toMatchSnapshot();
+  });
+});
