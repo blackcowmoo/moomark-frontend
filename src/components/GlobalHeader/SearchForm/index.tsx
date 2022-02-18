4719cc0c1e677e53b2a6fc4fcf8745b7854b741c
Park <pjane123@naver.com>
2022-02-18 19:27:23 +0900

    Refactor SearchForm

--- src/components/GlobalHeader/SearchForm/index.tsx
+++ src/components/GlobalHeader/SearchForm/index.tsx
@@ -1,10 +1,17 @@
+import useInput from 'utils/hooks/useInput';
 import styles from './SearchForm.module.scss';
 
-const SearchForm = () => {
+interface ISearchForm {
+  searchInput?: string;
+}
+
+const SearchForm: React.FC<ISearchForm> = ({ searchInput }) => {
+  const [value, _, handleInput] = useInput<string>(searchInput || '');
+
   return (
     <div className={styles.container}>
       <form action='#' className={styles.search}>
-        <input type='text' className={styles.search__input} placeholder='Search input' />
+        <input type='text' className={styles.search__input} value={value} onChange={handleInput} placeholder='Search input' />
       </form>
     </div>
   );
