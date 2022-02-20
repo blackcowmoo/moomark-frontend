d0eca01fc5eca429af7fb6ddf8051a185633fdfe
Park <pjane123@naver.com>
2022-02-20 21:15:33 +0900

    Update Pagination.tsx

--- src/components/PostList/TablePostList/Pagination.tsx
+++ src/components/PostList/TablePostList/Pagination.tsx
@@ -17,7 +17,7 @@ const Pagination: React.FC<IPagination> = ({ totalPostCount, pageRange, currentP
   }
 
   const handlePaginate = (input: number) => {
-    // route.push({ query: { page: input } });
+    router.push({ query: { ...router.query, page: input } }, undefined, { shallow: true });
     setPage(input);
   };
 
@@ -26,16 +26,7 @@ const Pagination: React.FC<IPagination> = ({ totalPostCount, pageRange, currentP
       {pageNumbers.map((number) => {
         return (
           <button className={currentPage === number ? styles.activePage : styles.page} key={number} onClick={() => handlePaginate(number)}>
-            <Link
-              href={{
-                pathname: router.pathname,
-                query: { ...router.query, page: number },
-              }}
-              shallow
-              replace
-            >
-              <a>{number}</a>
-            </Link>
+            {number}
           </button>
         );
       })}
