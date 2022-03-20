30f894cce6eb18a9a3742363165e83fa23016e4a
GyeongSu Han <micalgenus@gmail.com>
2022-03-20 20:02:53 +0900

    Add log

--- src/components/LoginForm/GoogleLogin/GoogleSocialLogin.tsx
+++ src/components/LoginForm/GoogleLogin/GoogleSocialLogin.tsx
@@ -45,7 +45,7 @@ const GoogleSocialLogin: React.FC<props> = ({ onClose }) => {
   }, [data]);
 
   const onSuccess = (response: any) => {
-    console.log(customHeader);
+    console.log(customHeader, response);
     login({ variables: { type: 'Google', code: response.code, state: response.state } });
     onClose();
   };
