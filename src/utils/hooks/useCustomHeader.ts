78fe6edbb242e381ffe59bcbc2d76f34fc9b33e3
Park <pjane123@naver.com>
2022-03-22 21:28:22 +0900

    Add useCustomHeader

---
+++ src/utils/hooks/useCustomHeader.ts
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { customHeaderState } from 'recoil/customHeader';
+
+export default function useCustomHeader() {
+  const [customHeader, setCustomHeader] = useRecoilState(customHeaderState);
+
+  const LOCAL_STORAGE_KEY = 'mooMark-dev-customHeader';
+
+  useEffect(() => {
+    const savedCustomHeader = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (savedCustomHeader) {
+      const parsedHeader = JSON.parse(savedCustomHeader);
+      setCustomHeader(parsedHeader);
+    }
+  }, []);
+
+  return { customHeader, setCustomHeader };
+}
