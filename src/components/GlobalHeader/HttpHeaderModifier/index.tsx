be0593f81be9d9440b017f1b704dad8100e38ee1
Park Jein <pjane123@naver.com>
2022-03-24 22:10:18 +0900

    Fix custom header apply (#44)
    * Add useCustomHeader
    
    * Fix CustomHeader apply error

--- src/components/GlobalHeader/HttpHeaderModifier/index.tsx
+++ src/components/GlobalHeader/HttpHeaderModifier/index.tsx
@@ -1,10 +1,14 @@
 import Modal from '@components/common/Modal';
 import { useModal } from 'utils/hooks/useModal';
+import useCustomHeader from 'utils/hooks/useCustomHeader';
 import HttpHeaderModifier from './HttpHeaderModifier';
+
 import styles from './HttpHeaderModifier.module.scss';
 
 const index = () => {
+  useCustomHeader();
   const { isShown, toggle } = useModal();
+
   return (
     <div>
       <button className={styles.button} onClick={toggle}>
