c48d2561bee4c78ce4dfdbf9d763c0bff85b96b6
GyeongSu Han <micalgenus@gmail.com>
2022-03-26 21:55:43 +0900

    Fix graphql query

--- src/api/queries/auth.queries.ts
+++ src/api/queries/auth.queries.ts
@@ -2,6 +2,9 @@ import { gql } from '@apollo/client';
 
 export const LOGIN = gql`
   mutation LogIn($type: String!, $code: String!) {
-    login(type: $type, code: $code) 
+    login(type: $type, code: $code) {
+      token
+      refreshToken
+    }
   }
 `;
