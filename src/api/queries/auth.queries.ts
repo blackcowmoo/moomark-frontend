40b9d1e1cd5b69f3df2f7f31372b11b805974ad6
Park Jein <pjane123@naver.com>
2022-05-14 19:46:41 +0900

    GraphQL Social Login (#18)
    * style: remove unused style
    
    * refactor: AppLayout 폴더 이동
    
    * feat: GoogleSocialLogin Graphql 추가
    
    * Lint Fix
    
    * Add state
    
    * Add log
    
    * Remove unused paremeter
    
    * Fix graphql query
    
    * Add user type
    
    Co-authored-by: GyeongSu Han <micalgenus@gmail.com>

--- src/api/queries/auth.queries.ts
+++ src/api/queries/auth.queries.ts
@@ -2,6 +2,16 @@ import { gql } from '@apollo/client';
 
 export const LOGIN = gql`
   mutation LogIn($type: String!, $code: String!) {
-    login(type: $type, code: $code) 
+    login(type: $type, code: $code) {
+      token
+      refreshToken
+      user {
+        name
+        email
+        nickname
+        picture
+        role
+      }
+    }
   }
 `;
--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import GlobalHeader from '../GlobalHeader';
+
 import styles from './AppLayout.module.scss';
 
 const AppLayout: React.FC<ReactNode> = ({ children }) => {
