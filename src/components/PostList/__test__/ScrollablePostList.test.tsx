82a9f6ed88c26808a1f343c798919c4c5459aa51
pjainxido <pjainxido@gmail.com>
2022-07-03 17:06:57 +0900

    Fix ScrollablePost test codes

--- src/components/PostList/__test__/ScrollablePostList.test.tsx
+++ src/components/PostList/__test__/ScrollablePostList.test.tsx
@@ -1,10 +1,26 @@
+import { MockedProvider } from '@apollo/client/testing';
 import { mount } from 'enzyme';
+
 import { HomePageListMock } from 'utils/mock';
 import ScrollablePostList from '../ScrollablePostList';
 
 describe('<ScrollablePostList/>', () => {
+  beforeEach(() => {
+    // IntersectionObserver isn't available in test environment
+    const mockIntersectionObserver = jest.fn();
+    mockIntersectionObserver.mockReturnValue({
+      observe: () => null,
+      unobserve: () => null,
+      disconnect: () => null,
+    });
+    window.IntersectionObserver = mockIntersectionObserver;
+  });
   it('matches snapshot', () => {
-    const wrapper = mount(<ScrollablePostList listTitle='테스트' preRenderPosts={HomePageListMock} />);
+    const wrapper = mount(
+      <MockedProvider>
+        <ScrollablePostList listTitle='테스트' preRenderPosts={HomePageListMock} />
+      </MockedProvider>,
+    );
     expect(wrapper.debug()).toMatchSnapshot();
   });
 });
