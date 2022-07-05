7a1530cbfb35dafe5062ec67a13f08cfaf825840
pjainxido <pjainxido@gmail.com>
2022-07-05 21:26:00 +0900

    Change DrodownMenu menu list

--- src/components/GlobalHeader/UserMenu/DropdownMenu/index.tsx
+++ src/components/GlobalHeader/UserMenu/DropdownMenu/index.tsx
@@ -18,9 +18,7 @@ const DropdownMenu: React.FC<IDropdownMenu> = ({ userName, setLogOut }) => {
       <div className={styles.menuWrapper}>
         <div className={styles.userInfo}>{userName}님 환영합니다!</div>
         <Link href='/'>마이페이지</Link>
-        <Link href='/'>내 북마크</Link>
-        <Link href='/edit'>새 글 작성</Link>
-        <Link href='/'>임시글 리스트</Link>
+        <Link href='/edit'>글쓰기</Link>
         <Link href='/setting'>설정</Link>
         <div onClick={setLogOut}>
           <GoogleLogout clientId={GOOGLE_CLIENT_ID as string} onLogoutSuccess={setLogOut} render={() => <div className={styles.logout}>로그아웃</div>} />
